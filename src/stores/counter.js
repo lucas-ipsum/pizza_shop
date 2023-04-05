b29a63166e2243b5690146306bc4303f0d84de09
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-05 23:11:33 +0200

    store created

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+/*
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
@@ -9,4 +10,22 @@ export const useCounterStore = defineStore('counter', () => {
   }
 
   return { count, doubleCount, increment }
+})
+*/ 
+
+
+export const userCartStore = defineStore('cart',{
+  state: () => ({
+    style: 'Italian'
+  }),
+  getters: {
+    getCart: (state) => {
+      return state.cart
+    }
+  }, 
+  actions: {
+    addToCart() {
+      // this.cart
+    }
+  }
 })
