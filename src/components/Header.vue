60904d612238ec52c8a9d03a4a98f3f985995a07
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-05 17:53:22 +0200

    project setup

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <h1>Pizza App</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name : 'Header'
+    }
+</script>
---
+++ src/components/SelectionIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div >
+        <h1 class="text-3xl font-bold">Hallo</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'SelectionIcon'
+    }
+</script>
+
+<style scoped>
+ div {
+    margin: '5px';
+    background-color: aliceblue;
+    border-radius: 5%;
+    color: black;
+ }
+</style>
