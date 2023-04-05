bfd81c443001efdc2fbd29e89fba741fde820e5e
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-05 22:38:38 +0200

    topping icons added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
-  import Test from './components/Test.vue';
   import Header from './components/Header.vue';
-  import SelectionIcon from './components/SelectionIcon.vue'
-import PizzaStyle from './components/PizzaStyle.vue';
-import DoughType from './components/DoughType.vue';
+  import PizzaStyle from './components/PizzaStyle.vue';
+  import DoughType from './components/DoughType.vue';
+  import Toppings from './components/Toppings.vue';
 </script>
 
 <template>
   <Header /> 
-  <SelectionIcon /> 
   <PizzaStyle />
   <DoughType />
+  <Toppings />
 </template>
 
 <style scoped>
---
+++ src/components/Toppings.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1 class="category-title">Teigart auswählen</h1>
+    <hr>
+    <div class="wrapper">
+        <SelectionIcon title="Salami" logo="src/assets/icons/standard_dough.png"/>
+        <SelectionIcon title="Paprika" logo="src/assets/icons/whole_grain.png"/>
+        <SelectionIcon title="Knoblauch" logo="src/assets/icons/sour_dough.png"/>
+    </div>
+    <diw class="wrapper">
+        <SelectionIcon title="Käse" logo="src/assets/icons/sour_dough.png"/>
+        <SelectionIcon title="Mais" logo="src/assets/icons/sour_dough.png"/>
+    </diw>
+</template>
+
+<script>
+    import SelectionIcon from './SelectionIcon.vue';
+    export default {
+    name: "Toppings",
+    components: { SelectionIcon },
+
+}
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+        justify-content: space-evenly;
+        margin-top: 5px;
+    }
+    hr {
+        color: bisque;
+    }
+    .category-title {
+        margin-top: 25px;
+        text-align: left;
+        font-size: larger;
+    }
+</style>
