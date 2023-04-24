56718faeac503dfcadcb3a34d254d8b64310b686
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-24 17:50:57 +0200

    cart store edited

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,29 +1,8 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-/*
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
 
-  return { count, doubleCount, increment }
-})
-*/ 
-
-
-export const userCartStore = defineStore('cartList',{
+export const userCartStore = defineStore('cartStore',{
   state: () => ({
-    cartList: []
-  }),
-  actions: {
-    addToCart(item) {
-      // const created = {style: style};
-      // this.cart = [created, ...this.cart]
-      // this.cart
-      this.cartList.push({item})
-    }
-  }
+    cart: {toppings: 'Cheese'}
+  })
 })
